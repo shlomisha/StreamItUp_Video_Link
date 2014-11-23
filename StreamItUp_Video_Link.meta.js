f228e45922f55ca000e22fb5064f8739ccf40d0e
shlomisha <shlomi.shasha@gmail.com>
2014-11-24 00:03:19 +0200

    Added files. ver 1.1.0

---
+++ StreamItUp_Video_Link.meta.js
@@ -0,0 +1,15 @@
+// ==UserScript==
+// @id             HUJI_Stream_It_Up_Video_Link
+// @name           StreamItUp Video Link
+// @version        1.1.0
+// @namespace      shlomisha
+// @author         shlomisha
+// @description    Shows the lectures links, in streamitup.com systems, so it is easier to download them, or watch them in other OS than Windows
+// @include        http://www.streamitup.com/elearning/*/vod_student_window_E.php
+// @include        http://www.streamitup.com/huji_db/*
+// @include        http://www.streamitupcdn.net/huji_db/database/vod_student_window_E.php
+// @grant          GM_addStyle
+// @run-at         document-end
+// @updateURL      https://raw.githubusercontent.com/shlomisha/userscripts/master/StreamItUp_Video_Link.meta.js
+// @downloadURL    https://raw.githubusercontent.com/shlomisha/userscripts/master/StreamItUp_Video_Link.user.js
+// ==/UserScript==
