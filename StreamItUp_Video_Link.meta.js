5cef4d7ad343704c4769336755e5a8f082b71638
Shlomi <shlomisha@users.noreply.github.com>
2015-11-24 15:17:40 +0200

    Update StreamItUp_Video_Link.meta.js
    + Added support for Ein-Karem lectures (ekmd_db subfolder)
    + Added support for rtsp: Removing it and refer the user to use http instead

--- StreamItUp_Video_Link.meta.js
+++ StreamItUp_Video_Link.meta.js
@@ -1,13 +1,14 @@
 // ==UserScript==
 // @id             HUJI_Stream_It_Up_Video_Link
 // @name           StreamItUp Video Link
-// @version        1.1.1
+// @version        1.1.2
 // @namespace      shlomisha
 // @author         shlomisha
 // @description    Shows the lectures links, in streamitup.com systems, so it is easier to download them, or watch them in other OS than Windows
 // @include        http://www.streamitup.com/elearning/*/vod_student_window_E.php
 // @include        http://www.streamitup.com/huji_db/*
 // @include        http://www.streamitupcdn.net/huji_db/database/vod_student_window_E.php
+// @include        http://www.streamitupcdn.net/ekmd_db/database/vod_student_window_E.php
 // @grant          GM_addStyle
 // @run-at         document-end
 // @updateURL      https://raw.githubusercontent.com/shlomisha/StreamItUp_Video_Link/master/StreamItUp_Video_Link.meta.js
